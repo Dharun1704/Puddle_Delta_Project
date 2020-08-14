1016544ec0953fe819c7075e001b26c166ff0f82
Dharun Vaibhav <dharunvaibhav@gmail.com>
2020-08-14 15:20:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/deltaproject/ProfileActivity.java
@@ -0,0 +1,396 @@
+package com.example.deltaproject;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Objects;
+
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
+    private LinearLayout dvlModePassword, dvlModeMain;
+    ArrayAdapter<String> theme;
+    String[] fTheme;
+    int np;
+    int selected[] = {0};
+    boolean isDeveloper;
+
+    TextView appTitle, userLvl, newsPoint, newsTheme;
+    RelativeLayout NewsThemeLayout;
+    EditText dvlPassword, dvlCustomNp;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#5098A9"));
+        window.setNavigationBarColor(Color.parseColor("#205888"));
+
+        toolbar.setBackgroundColor(Color.parseColor("#5098A9"));
+
+        appTitle = findViewById(R.id.appName);
+        appTitle.setText("Puddle");
+        appTitle.setTextColor(Color.WHITE);
+
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawerLayout = findViewById(R.id.profile_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        dvlModePassword = findViewById(R.id.developerModePassword);
+        dvlModeMain = findViewById(R.id.developerModeMain);
+        NewsThemeLayout = findViewById(R.id.newsTheme);
+        newsTheme = findViewById(R.id.newsThemeText);
+        userLvl = findViewById(R.id.profile_level);
+        newsPoint = findViewById(R.id.profile_np);
+        dvlPassword = findViewById(R.id.developerPassword);
+        dvlCustomNp = findViewById(R.id.customNP);
+        dvlCustomNp.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId2, KeyEvent event2) {
+                if (actionId2 == EditorInfo.IME_ACTION_SEARCH || actionId2 == EditorInfo.IME_ACTION_DONE ||
+                        event2 != null &&
+                                event2.getAction() == KeyEvent.ACTION_DOWN &&
+                                event2.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    if (event2 == null || !event2.isShiftPressed()) {
+
+                        np = Integer.parseInt(view.getText().toString());
+                        SharedPreferences newsPoints = getSharedPreferences("newsPoints", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = newsPoints.edit();
+                        editor.putInt("np", np);
+                        editor.apply();
+                        newsPoint.setText(String.valueOf(np));
+                        assignLevel(np);
+                        dvlCustomNp.setText("");
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        SharedPreferences developer = getSharedPreferences("Developer", Context.MODE_PRIVATE);
+        isDeveloper = developer.getBoolean("isDeveloper", false);
+
+        if (isDeveloper) {
+            dvlModeMain.setVisibility(View.VISIBLE);
+        }
+        else {
+            dvlModeMain.setVisibility(View.GONE);
+            dvlModePassword.setVisibility(View.GONE);
+        }
+
+        SharedPreferences newsPoints = getSharedPreferences("newsPoints", Context.MODE_PRIVATE);
+        np = newsPoints.getInt("np", 0);
+
+        assignLevel(np);
+        newsPoint.setText(String.valueOf(np));
+
+        fTheme = new String[2];
+        fTheme[0] = "Violet-Blue";
+        fTheme[1] = "Dark";
+
+        SharedPreferences sNewsTheme = getSharedPreferences("sNewsTheme", MODE_PRIVATE);
+        fTheme[0] = sNewsTheme.getString("fNewsTheme", "Violet-Blue");
+
+        newsTheme.setText(fTheme[0]);
+
+        SharedPreferences Selected = getSharedPreferences("NewsTheme", MODE_PRIVATE);
+        selected[0] = Selected.getInt("OptionSelectedNewsTheme", 0);
+
+        theme = new ArrayAdapter<>(ProfileActivity.this, R.layout.dialog_item);
+        theme.add("Violet - Blue");
+        theme.add("Deep Sea");
+        theme.add("Dark");
+
+        NewsThemeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences Selected = getSharedPreferences("NewsTheme", MODE_PRIVATE);
+                selected[0] = Selected.getInt("OptionSelectedNewsTheme", 0);
+
+                new AlertDialog.Builder(ProfileActivity.this)
+                        .setTitle("Select News Theme")
+                        .setSingleChoiceItems(theme, selected[0], new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                selected[0] = which;
+
+                                SharedPreferences Selected = getSharedPreferences("NewsTheme", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = Selected.edit();
+                                editor.putInt("OptionSelectedNewsTheme", selected[0]);
+                                editor.apply();
+                            }
+                        })
+                        .setPositiveButton("Apply", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                fTheme[0] = theme.getItem(selected[0]);
+
+                                SharedPreferences sNewsTheme = getSharedPreferences("sNewsTheme", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sNewsTheme.edit();
+                                editor.putString("fNewsTheme", fTheme[0]);
+                                editor.apply();
+
+                                newsTheme.setText(fTheme[0]);
+                                checkLvlFeatures();
+                            }
+                        })
+                        .show();
+            }
+
+        });
+
+    }
+
+    private void assignLevel(int nps) {
+        if (nps >= 0 && nps <= 50) {
+            userLvl.setText("Noob Master");
+        }
+        else if (nps > 50 && nps <= 100) {
+            userLvl.setText("Reader");
+        }
+        else if (nps > 100 && nps <= 300) {
+            userLvl.setText("Front Bencher");
+        }
+        else if (nps > 300 && nps <= 500) {
+            userLvl.setText("Prodigy");
+        }
+        else if (nps > 500 ) {
+            userLvl.setText("News Wizard");
+        }
+        else if (isDeveloper) {
+            userLvl.setText("Developer");
+        }
+    }
+
+    private void checkLvlFeatures() {
+        String s = newsTheme.getText().toString();
+        if (np < 51 && s.equals("Deep Sea")) {
+            Toast.makeText(ProfileActivity.this, "Theme unavailable for this level. Current theme set is the latest theme available", Toast.LENGTH_LONG).show();
+            selected[0] = 0;
+            SharedPreferences Selected = getSharedPreferences("NewsTheme", MODE_PRIVATE);
+            SharedPreferences.Editor editorS = Selected.edit();
+            editorS.putInt("OptionSelectedNewsTheme", selected[0]);
+            editorS.apply();
+            fTheme[0] = theme.getItem(selected[0]);
+            SharedPreferences sNewsTheme = getSharedPreferences("sNewsTheme", MODE_PRIVATE);
+            SharedPreferences.Editor editorT = sNewsTheme.edit();
+            editorT.putString("fNewsTheme", fTheme[0]);
+            editorT.apply();
+
+            newsTheme.setText(fTheme[0]);
+        }
+
+        if (np < 51 && s.equals("Dark")) {
+            Toast.makeText(ProfileActivity.this, "Theme unavailable for this level. Current theme set is the latest theme available", Toast.LENGTH_LONG).show();
+            selected[0] = 0;
+            SharedPreferences Selected = getSharedPreferences("NewsTheme", MODE_PRIVATE);
+            SharedPreferences.Editor editorS = Selected.edit();
+            editorS.putInt("OptionSelectedNewsTheme", selected[0]);
+            editorS.apply();
+            fTheme[0] = theme.getItem(selected[0]);
+            SharedPreferences sNewsTheme = getSharedPreferences("sNewsTheme", MODE_PRIVATE);
+            SharedPreferences.Editor editorT = sNewsTheme.edit();
+            editorT.putString("fNewsTheme", fTheme[0]);
+            editorT.apply();
+
+            newsTheme.setText(fTheme[0]);
+        }
+
+        if (np < 101 && s.equals("Dark")) {
+            Toast.makeText(ProfileActivity.this, "Theme unavailable for this level. Current theme set is the latest theme available", Toast.LENGTH_LONG).show();
+            selected[0] = 1;
+            SharedPreferences Selected = getSharedPreferences("NewsTheme", MODE_PRIVATE);
+            SharedPreferences.Editor editorS = Selected.edit();
+            editorS.putInt("OptionSelectedNewsTheme", selected[0]);
+            editorS.apply();
+            fTheme[0] = theme.getItem(selected[0]);
+            SharedPreferences sNewsTheme = getSharedPreferences("sNewsTheme", MODE_PRIVATE);
+            SharedPreferences.Editor editorT = sNewsTheme.edit();
+            editorT.putString("fNewsTheme", fTheme[0]);
+            editorT.apply();
+
+            newsTheme.setText(fTheme[0]);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+
+        else if (item.getItemId() == R.id.profile_reset) {
+            np = 0;
+            SharedPreferences newsPoints = getSharedPreferences("newsPoints", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = newsPoints.edit();
+            editor.putInt("np", np);
+            editor.apply();
+            newsPoint.setText(String.valueOf(np));
+            isDeveloper = false;
+            SharedPreferences developer = getSharedPreferences("Developer", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editorD = developer.edit();
+            editorD.putBoolean("isDeveloper", isDeveloper);
+            editorD.apply();
+            selected[0] = 0;
+            SharedPreferences Selected = getSharedPreferences("NewsTheme", MODE_PRIVATE);
+            SharedPreferences.Editor editorS = Selected.edit();
+            editorS.putInt("OptionSelectedNewsTheme", selected[0]);
+            editorS.apply();
+            fTheme[0] = theme.getItem(selected[0]);
+            SharedPreferences sNewsTheme = getSharedPreferences("sNewsTheme", MODE_PRIVATE);
+            SharedPreferences.Editor editorT = sNewsTheme.edit();
+            editorT.putString("fNewsTheme", fTheme[0]);
+            editorT.apply();
+
+            newsTheme.setText(fTheme[0]);
+            Intent i = getIntent();
+            startActivity(i);
+            finish();
+        }
+
+        else if (item.getItemId() == R.id.developMode) {
+            dvlModePassword.setVisibility(View.VISIBLE);
+            dvlPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE ||
+                            event != null &&
+                                    event.getAction() == KeyEvent.ACTION_DOWN &&
+                                    event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                        if (event == null || !event.isShiftPressed()) {
+                            String password = v.getText().toString();
+                            if (password.equals("puddle")) {
+
+                                isDeveloper = true;
+                                SharedPreferences developer = getSharedPreferences("Developer", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editorD = developer.edit();
+                                editorD.putBoolean("isDeveloper", isDeveloper);
+                                editorD.apply();
+
+                                dvlModePassword.setVisibility(View.GONE);
+                                dvlModeMain.setVisibility(View.VISIBLE);
+
+                                dvlCustomNp.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                                    @Override
+                                    public boolean onEditorAction(TextView view, int actionId2, KeyEvent event2) {
+                                        if (actionId2 == EditorInfo.IME_ACTION_SEARCH || actionId2 == EditorInfo.IME_ACTION_DONE ||
+                                                event2 != null &&
+                                                        event2.getAction() == KeyEvent.ACTION_DOWN &&
+                                                        event2.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                                            if (event2 == null || !event2.isShiftPressed()) {
+
+                                                np = Integer.parseInt(view.getText().toString());
+                                                SharedPreferences newsPoints = getSharedPreferences("newsPoints", Context.MODE_PRIVATE);
+                                                SharedPreferences.Editor editor = newsPoints.edit();
+                                                editor.putInt("np", np);
+                                                editor.apply();
+                                                newsPoint.setText(String.valueOf(np));
+                                                assignLevel(np);
+                                                dvlCustomNp.setText("");
+                                                return true;
+                                            }
+                                        }
+                                        return false;
+                                    }
+                                });
+
+                            }
+                            else {
+                                Snackbar.make(drawerLayout, "Incorrect Password", Snackbar.LENGTH_LONG).show();
+                                dvlModePassword.setVisibility(View.GONE);
+                            }
+
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            });
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        Intent i;
+        switch (id) {
+            case R.id.news:
+                i = new Intent(this, NewsActivity.class);
+                startActivity(i);
+                finish();
+                break;
+            case R.id.weather:
+                i = new Intent(this, WeatherActivity.class);
+                startActivity(i);
+                finish();
+                break;
+            case R.id.profile:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.settings:
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/deltaproject/SensorActivity.java
@@ -0,0 +1,259 @@
+package com.example.deltaproject;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class SensorActivity extends AppCompatActivity implements SensorEventListener{
+
+    TextView mDate, mYear, mTemp, mHum, mLight, mPressure, uTemp, uPress, uHum;
+    TextView[] mAcc, uAcc, mGyro, mGravity, uGravity, mGmf;
+    Sensor sPressure, sHumidity, sLight, sTemperature, sAccelerometer, sGyroscope, sGravity, sGMF;
+    SensorManager manager;
+
+    private BroadcastReceiver mBattery = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra("level",0);
+            TextView battery = findViewById(R.id.batteryLvl);
+            battery.setText(level + "%");
+        }
+    };
+
+    @Override
+    @SuppressLint("SimpleDateFormat")
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mDate = findViewById(R.id.sensorDate);
+        mYear = findViewById(R.id.sensorYear);
+        mTemp = findViewById(R.id.sensorTemp);
+        mHum = findViewById(R.id.sensorHumidity);
+        mLight = findViewById(R.id.sensorLight);
+        mPressure = findViewById(R.id.sensorPressure);
+
+        uTemp = findViewById(R.id.tempUnit);
+        uPress = findViewById(R.id.pressUnit);
+        uHum = findViewById(R.id.humUnit);
+
+        mAcc = new TextView[3];
+        mAcc[0] = findViewById(R.id.acc_x);
+        mAcc[1] = findViewById(R.id.acc_y);
+        mAcc[2] = findViewById(R.id.acc_z);
+
+        uAcc = new TextView[3];
+        uAcc[0] = findViewById(R.id.acc_xUnit);
+        uAcc[1] = findViewById(R.id.acc_yUnit);
+        uAcc[2] = findViewById(R.id.acc_zUnit);
+        for (int i = 0; i < 3; i++) {
+            uAcc[i].setText(Html.fromHtml("m/s<sup>2</sup>"));
+        }
+
+        mGyro = new TextView[3];
+        mGyro[0] = findViewById(R.id.gyro_x);
+        mGyro[1] = findViewById(R.id.gyro_y);
+        mGyro[2] = findViewById(R.id.gyro_z);
+
+        mGravity = new TextView[3];
+        mGravity[0] = findViewById(R.id.grv_x);
+        mGravity[1] = findViewById(R.id.grv_y);
+        mGravity[2] = findViewById(R.id.grv_z);
+
+        uGravity = new TextView[3];
+        uGravity[0] = findViewById(R.id.grv_xUnit);
+        uGravity[1] = findViewById(R.id.grv_yUnit);
+        uGravity[2] = findViewById(R.id.grv_zUnit);
+        for (int i = 0; i < 3; i++) {
+            uGravity[i].setText(Html.fromHtml("m/s<sup>2</sup>"));
+        }
+
+        mGmf = new TextView[3];
+        mGmf[0] = findViewById(R.id.gmf_x);
+        mGmf[1] = findViewById(R.id.gmf_y);
+        mGmf[2] = findViewById(R.id.gmf_z);
+
+
+        Date date = new Date();
+        Timestamp ts = new Timestamp(date.getTime());
+        SimpleDateFormat format1 = new SimpleDateFormat("dd MMMM");
+        SimpleDateFormat format2 = new SimpleDateFormat("YYYY");
+        mDate.setText(format1.format(ts));
+        mYear.setText(format2.format(ts));
+
+        registerReceiver(mBattery, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        setSensors();
+
+
+    }
+
+    @SuppressLint({"DefaultLocale", "SetTextI18n"})
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            mTemp.setText(String.valueOf((int) event.values[0]));
+        }
+
+        if (sensor.getType() == Sensor.TYPE_PRESSURE) {
+            mPressure.setText(String.valueOf((int) event.values[0]));
+        }
+
+        if (sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY) {
+            mHum.setText(String.valueOf((int) event.values[0]));
+        }
+
+        if (sensor.getType() == Sensor.TYPE_LIGHT) {
+            mLight.setText(String.format("%.1f", event.values[0]));
+        }
+
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            mAcc[0].setText(String.format("%.5f", event.values[0]) + " ");
+            mAcc[1].setText(String.format("%.5f", event.values[1]) + " ");
+            mAcc[2].setText(String.format("%.5f", event.values[2]) + " ");
+        }
+
+        if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            mGyro[0].setText(String.format("%.5f", event.values[0]) + " rad/s");
+            mGyro[1].setText(String.format("%.5f", event.values[1]) + " rad/s");
+            mGyro[2].setText(String.format("%.5f", event.values[2]) + " rad/s");
+        }
+
+        if (sensor.getType() == Sensor.TYPE_GRAVITY) {
+            mGravity[0].setText(String.format("%.4f", event.values[0]) + " ");
+            mGravity[1].setText(String.format("%.4f", event.values[1]) + " ");
+            mGravity[2].setText(String.format("%.4f", event.values[2]) + " ");
+        }
+
+        if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mGmf[0].setText(String.format("%.5f", event.values[0]) + " µT");
+            mGmf[1].setText(String.format("%.5f", event.values[1]) + " µT");
+            mGmf[2].setText(String.format("%.5f", event.values[2]) + " µT");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void setSensors() {
+
+        sTemperature = manager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if (sTemperature != null) {
+            manager.registerListener(SensorActivity.this, sTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            mTemp.setText(R.string.sensorError);
+            mTemp.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+            mTemp.setTextColor(Color.RED);
+            uTemp.setVisibility(View.GONE);
+        }
+
+        sPressure = manager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if (sPressure != null) {
+            manager.registerListener(SensorActivity.this, sPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            mPressure.setText(R.string.sensorError);
+            mPressure.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+            mPressure.setTextColor(Color.RED);
+            uPress.setVisibility(View.GONE);
+        }
+
+        sLight = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (sLight != null) {
+            manager.registerListener(SensorActivity.this, sLight, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            mLight.setText(R.string.sensorError);
+            mLight.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+            mLight.setTextColor(Color.RED);
+        }
+
+        sHumidity = manager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        if (sHumidity != null) {
+            manager.registerListener(SensorActivity.this, sHumidity, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            mHum.setText(R.string.sensorError);
+            mHum.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+            mHum.setTextColor(Color.RED);
+            uHum.setVisibility(View.GONE);
+        }
+
+        sAccelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (sAccelerometer != null) {
+            manager.registerListener(SensorActivity.this, sAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            mAcc[0].setText(R.string.sensorError);
+            mAcc[0].setTextColor(Color.RED);
+            uAcc[0].setVisibility(View.GONE);
+            for (int i = 1; i < 3; i++) {
+                mAcc[i].setVisibility(View.GONE);
+                uAcc[i].setVisibility(View.GONE);
+            }
+        }
+
+        sGyroscope = manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (sGyroscope != null) {
+            manager.registerListener(SensorActivity.this, sGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            mGyro[0].setText(R.string.sensorError);
+            mGyro[0].setTextColor(Color.RED);
+            mGyro[1].setVisibility(View.GONE);
+            mGyro[2].setVisibility(View.GONE);
+        }
+
+        sGravity = manager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        if (sGravity != null) {
+            manager.registerListener(SensorActivity.this, sGravity, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            mGravity[0].setText(R.string.sensorError);
+            mGravity[0].setTextColor(Color.RED);
+            uGravity[0].setVisibility(View.GONE);
+            for (int i = 1; i < 3; i++) {
+                mGravity[i].setVisibility(View.GONE);
+                uGravity[i].setVisibility(View.GONE);
+            }
+        }
+
+        sGMF = manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (sGMF != null) {
+            manager.registerListener(SensorActivity.this, sGMF, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            mGmf[0].setText(R.string.sensorError);
+            mGmf[0].setTextColor(Color.RED);
+            mGmf[1].setVisibility(View.GONE);
+            mGmf[2].setVisibility(View.GONE);
+        }
+    }
+}
